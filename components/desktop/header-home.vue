ab5e7237c8cf39e73e5057c3918b0db2b639ebcf
machien-hiko88 <chien.mv@hikotech.io>
2023-01-03 22:45:52 +0700

    add header footer

---
+++ components/desktop/header-home.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="header-home">
+    <div class="ctn-logo">
+      <img class="logo" src="~/assets/images/ic-logo.png" />
+    </div>
+    <div class="ctn-btn">
+      <p class="txt-btn">Liên kết ví</p>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+.header-home {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  position: absolute;
+  width: 100%;
+  height: 80px;
+  top: 0px;
+  background: #ffffff;
+  border-bottom: 1px solid #f4f4f5;
+  .ctn-logo {
+    margin-left: 24px;
+    .logo {
+      width: 122px;
+      height: 32px;
+    }
+  }
+  .ctn-btn {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-right: 24px;
+    width: 160px;
+    height: 48px;
+    background: #faac2e;
+    border-radius: 8px;
+    :hover{
+        cursor: pointer;
+    }
+    .txt-btn {
+      font-weight: 500;
+      font-size: 14px;
+      line-height: 20px;
+      color: #18181b;
+    }
+  }
+}
+</style>
--- components/desktop/index.vue
+++ components/desktop/index.vue
@@ -1,5 +1,16 @@
 <template>
-    <div>
-        <p>desktop</p>
-    </div>
+  <div>
+    <headerHome></headerHome>
+    <p>desktop</p>
+    <footerHome></footerHome>
+  </div>
 </template>
+<script>
+import { defineAsyncComponent } from "vue";
+export default {
+  components: {
+    headerHome: defineAsyncComponent(() => import("./header-home.vue")),
+    footerHome: defineAsyncComponent(() => import("./footer-home.vue")),
+  }
+};
+</script>
